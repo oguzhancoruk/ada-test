16bb4ffcc73609448f22d4b3e221a283dd23a8f4
oguzhan coruk <coruk_61@hotmail.com>
2023-01-30 18:00:27 +0300

    pushh

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -51,7 +51,7 @@ function User() {
 
 
                         <div className='user-div'>
-                            <button disabled={disabled} className=' btn btn-primary' ><a className='user-btn btn btn-primary ' href="home">Teste Başla</a></button>
+                            <button disabled={disabled} className=' btn btn-primary' ><a className='user-btn btn btn-primary ' href="/home">Teste Başla</a></button>
                         </div>
 
                     </form>
